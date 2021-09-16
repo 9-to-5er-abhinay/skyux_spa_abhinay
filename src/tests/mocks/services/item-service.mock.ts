3e8eff224a9e4a30cc7280dd5002800f8bda946a
Abhinay Dhiman <abhinay.dhiman@nagarro.com>
2021-09-16 22:26:07 +0530

    Added unit tests

--- src/tests/mocks/services/item-service.mock.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs/internal/Observable';
-import { of } from 'rxjs/internal/observable/of';
-
-export class ItemServiceMock {
-
-  public getItems(): Observable<[]> {
-    return of([]);
-  }
-}
